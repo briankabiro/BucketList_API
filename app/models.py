3754e872199baaed11894779c808d071e04a7905
bkabiro <briankigondu@gmail.com>
2017-08-04 18:19:02 +0300

    Add Methods to Models
    I added several methods to the methods. Fixed the db.Model parameter

--- app/models.py
+++ app/models.py
@@ -9,12 +9,12 @@ class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(50), unique=True)
 	password = 	db.Column(db.String)
-
+	date_created = db.Column(db.DateTime, default = db.func.current_timestamp())
 	def __repr__(self):
 		# return the name of the User
 		return '<User %s>' % self.username
 
-class Bucket(Model):
+class Bucket(db.Model):
 	""" 
 		Class for a creating a bucket object
 		Attributes:
@@ -25,20 +25,55 @@ class Bucket(Model):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(50))
 	items = db.Column()
+	date_created = db.Column(db.DateTime, default= db.func.current_timestamp())
+	date_modified = db.Column(db.DateTime, default= db.func.current_timestamp(),onupdate=db.func.current_timestamp())
+	
+	def save(self):
+		# save a bucket to the table
+		db.session.add(self)
+		db.session.commit()
+
+	def get_all():
+		# return all the buckets in table
+		return Bucket.query.all()
+
+	def delete(self):
+		# delete a bucket from the table
+		db.session.delete(self)
+		db.commit()
+
 	def __repr__(self):
 		# return the name of the bucket
-		return "Bucket %s>" % self.name
+		return "<Bucket %s>" % self.name
 
-class BucketItem(Model):
+class BucketItem(db.Model):
 	"""
 		Class for creating an item object
 		Attributes:
 			id: unique id for identifying each item
 			description: the text/description of an item
 	"""
+	
+	__table__ = "bucket_items"
 	id = db.Column(db.Integer, primary_key=True)
 	description = db.Column(db.Text)
+	date_created = db.Column(db.DateTime, default = db.func.current_timestamp())
+	date_modified = db.Column(db.DateTime, default= db.func.current_timestamp(),onupdate=db.func.current_timestamp())
+
+	def save(self):
+		# save an item to the table
+		db.session.add(self)
+		db.session.commit()
+
+	def get_all():
+		# return all the items in the table
+		return BucketItem.query.all()
+
+	def delete(self):
+		# delete an item from the table
+		db.session.delete(self)
+		db.commit()
 
 	def __repr__(self):
 		# return the description of an item
-		return "BucketItem %s>" % self.description
+		return "<BucketItem %s>" % self.description
