dda1687e54b260873bdb4352fbc1b1a61e34df04
bkabiro <briankigondu@gmail.com>
2017-08-05 21:13:26 +0300

    Add Instance Folder
    Added the instance folder which contains the configurations for the app

---
+++ instance/config.py
@@ -0,0 +1,27 @@
+import os
+
+class Config(object):
+	DEBUG = False
+	SECRET = os.getenv('SECRET')
+	SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+
+class DevelopmentConfig(Config):
+	# config for app when in development mode
+	DEBUG = True
+
+class TestingConfig(Config):
+	# config for the app when running tests
+	TESTING = True
+	DEBUG = True
+	SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URI')
+
+class ProductionConfig(Config):
+	# config for app when in production
+	DEBUG = False
+	TESTING = False
+
+app_config = {
+	'development': DevelopmentConfig,
+	'testing': TestingConfig,
+	'production': ProductionConfig
+}
