f16f821deb48404b36a2840037a366acb8b99b41
bkabiro <briankigondu@gmail.com>
2017-08-07 01:18:15 +0300

    Add Resources to Api
    I added the bucketlists and buckets classes to the API

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
 from instance.config import app_config
-from app.resources import BucketListApi
 db = SQLAlchemy()
 from app.models import User, Bucket, BucketItem
+from app.resources.bucketlist import BucketListApi, BucketListsApi
 
 def create_app(config_name):
 	app = Flask(__name__, instance_relative_config=True)
@@ -12,12 +12,13 @@ def create_app(config_name):
 	app.config.from_pyfile('config.py')
 	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 	db.init_app(app)
-	with app.app_context():
-		db.create_all()
+
 	return app
 
+app = create_app(config_name='development')
 api = Api(app)
-
 api.add_resource(BucketListApi, '/bucketlists/<id>')
+api.add_resource(BucketListsApi, '/bucketlists/')
+
 if __name__ == '__main__':
 	app.run(debug=True)
