b24b86d77ba3b020dc6c18dba6cb0d2e1a37abaa
bkabiro <briankigondu@gmail.com>
2017-08-09 22:03:39 +0300

    Refactor Imports
    I changed the api imports so that project can have different configs for testing and development

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
 from instance.config import app_config
 db = SQLAlchemy()
-from app.models import User, Bucket, BucketItem
-from app.resources.bucketlist import BucketListApi, BucketListsApi
 
 
 def create_app(config_name):
@@ -17,10 +15,5 @@ def create_app(config_name):
     return app
 
 
-app = create_app(config_name='development')
-api = Api(app)
-api.add_resource(BucketListApi, '/bucketlists/<id>')
-api.add_resource(BucketListsApi, '/bucketlists/')
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
--- run.py
+++ run.py
@@ -1,4 +1,12 @@
-from app import app
+from app import create_app
+from flask_restful import Api
+from app.models import User, Bucket, BucketItem
+from app.resources.bucketlist import BucketListApi, BucketListsApi
+
+app = create_app(config_name='development')
+api = Api(app)
+api.add_resource(BucketListApi, '/bucketlists/<id>')
+api.add_resource(BucketListsApi, '/bucketlists/')
 
 if __name__ == '__main__':
 	app.run()
