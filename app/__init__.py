414034fd47dd0dbad0bd4220818d76d56d4721bd
bkabiro <briankigondu@gmail.com>
2017-08-11 20:26:26 +0300

    Fix Imports
    I fixed the imports after I refactored the code for creating the app

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_restful import Api
 from instance.config import app_config
 db = SQLAlchemy()
-
+from app.models import User, Bucket, BucketItem
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
@@ -13,7 +12,6 @@ def create_app(config_name):
     db.init_app(app)
     db.create_all(app=app)
     return app
-
 
 if __name__ == '__main__':
     app.run()
