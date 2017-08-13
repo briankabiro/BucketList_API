4cbdd614664a86524a7f6fe72ba7bd3d2740da66
bkabiro <briankigondu@gmail.com>
2017-08-13 13:30:05 +0300

    Add Token and Password Hashing
    I added the password hashing functionality. Added jwt module for generating tokens

--- app/resources/auth.py
+++ app/resources/auth.py
@@ -1,6 +1,10 @@
 from flask_restful import Resource, abort, reqparse
 from flask import jsonify
+import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 from app.models import User
+import os
+import jwt
 parser = reqparse.RequestParser()
 
 def get_user(username):
@@ -8,13 +12,19 @@ def get_user(username):
 
 class Register(Resource):
 	def post(self):
+		parser.add_argument('username', required=True)
+		parser.add_argument('password', required=True)
 		args = parser.parse_args()
-		parser.add_argument('username')
-		parser.add_argument('password')
-		if get_user(args['username']) != None:
-			return jsonify({"message": "User already exists"})
-		user = User(username=args['username'], password=args['password'])
+		username, password = args['username'], generate_password_hash(args['password'])
+		
+		if get_user(username) != None:
+			response = jsonify({"message": "User already exists"})
+			response.status_code = 303
+			return response
+			
+		user = User(username=username, password=password)
 		user.save()
+		
 		response = jsonify({
 			"message":"Account successfully created"
 		})
@@ -23,29 +33,44 @@ class Register(Resource):
 
 class Login(Resource):
 	def post(self):
-		args = parser.parse_args()
 		parser.add_argument('username')
 		parser.add_argument('password')
-		user = get_user(args['username'])
-		if user:
-			if args['password'] == user.password:
-				#login the person
-				response = jsonify({"message": "Successfully logged in"})
-				response.status_code = 200
-		else:
-			return {"message": "Invalid credentials"}
+		args = parser.parse_args()
+		username, password = args['username'], args['password']
+		user = get_user(username)
 
+		if not user:
+			response =  jsonify({"message": "User doesn't exist"})
+			response.status_code = 401
+			return response
+
+		if check_password_hash(user.password, password):
+			#login the person
+			token = jwt.encode({'id': user.id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60)}, os.getenv('SECRET'))
+			response = jsonify({"message": "Successfully logged in", "token": token.decode('UTF-8')})
+			response.status_code = 200
+			return response
+
+		response = jsonify({"message": "Invalid password"})
+		response.status_code = 403
+		return response
+	
+			
 class ResetPassword(Resource):
 	def post(self):
-		args = parser.parse_args()
 		parser.add_argument('username')
-		parser.add_argument('password')	
+		parser.add_argument('password')
+		args = parser.parse_args()	
 		user = get_user(args['username'])
 		user.password = args['password']
 		user.save()
-		return {"message": "Password successfully changed"}
+		response = jsonify({"message": "Password successfully changed"})
+		response.status_code = 201
+		return response
 
 
 class Logout(Resource):
+	
 	def post(self):
 		user = get_user(args['username'])
+		# delete token
