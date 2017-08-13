db053343cab68a355d7c10d3ad879e17f83534bc
bkabiro <briankigondu@gmail.com>
2017-08-13 13:31:53 +0300

    Fix and Add Resources
    Included the resources to the Items Api and Item Api that handle endpoints to items/<item_id>

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ from app import create_app
 from flask_restful import Api
 from app.resources.bucketlist import BucketListApi, BucketListsApi
 from app.resources.auth import Register, Login, ResetPassword, Logout
+from app.resources.items import ItemApi, ItemsApi
 
 app = create_app(config_name='development')
 api = Api(app)
@@ -9,7 +10,9 @@ api.add_resource(BucketListApi, '/bucketlists/<id>')
 api.add_resource(BucketListsApi, '/bucketlists/')
 api.add_resource(Register, '/auth/register')
 api.add_resource(Login, '/auth/login')
-api.add_resource(ResetPassword, '/auth/reset_password')
+api.add_resource(ResetPassword, '/auth/reset-password')
+api.add_resource(ItemsApi, '/bucketlists/<id>/items/')
+api.add_resource(ItemApi, '/bucketlists/<id>/items/<item_id>')
 
 if __name__ == '__main__':
 	app.run()
