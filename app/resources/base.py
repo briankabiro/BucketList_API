1a017851656905bd0da3f73a0ad6c2a8e2bd00b4
bkabiro <briankigondu@gmail.com>
2017-08-17 17:47:54 +0300

    Fix Auth Decorator
    I fixed the auth decorator so that the user_id argument is passed correctly

--- app/resources/base.py
+++ app/resources/base.py
@@ -1,23 +1,24 @@
 from flask_restful import abort
 import jwt
-from flask import request
-from run import app
+from flask import request, current_app, json
+from functools import wraps
 
 def requires_auth(f):
 	@wraps(f)
 	def decorated(*args, **kwargs):
 		# check if token exists
-		auth = request.authorization
-		if not auth.token:
-			abort(401)
-		
+		auth_header = request.headers.get('Authorization')
+		if not auth_header:
+			abort(401)		
+		auth_token = auth_header.split(" ")[1]
+
 		try:
-			user_id = jwt.decode(auth.token, app.config.get('SECRET_KEY'))
-			
+			user_id_dict = jwt.decode(auth_token, current_app.config['SECRET_KEY'])
+			user_id = user_id_dict['id']
 		except jwt.ExpiredSignatureError:
 			return 'Signature Expired. Please log in again'
 		except jwt.InvalidTokenError:
 			return 'Invalid token. please login again'
 
-		return f(*args, **kwargs)
+		return f(*args, user_id, **kwargs)
 	return decorated
