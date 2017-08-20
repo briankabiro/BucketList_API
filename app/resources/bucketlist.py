b8b4a4602e78316d533d4bce869612c838a5cf60
bkabiro <briankigondu@gmail.com>
2017-08-20 20:21:06 +0300

    Add Serializers
    Added the serializers for the bucketlist and bucketlist item models to abstract the returned objects

--- app/resources/bucketlist.py
+++ app/resources/bucketlist.py
@@ -1,9 +1,9 @@
-from flask_restful import Resource, abort, reqparse
-from flask import jsonify, request
+from flask_restful import Resource, abort, reqparse, marshal
+from flask import jsonify, request, json
 from app.models import Bucketlist
 from app.resources.base import requires_auth
+from app.serializers.serializers import bucketlist_item_serializer, bucketlist_serializer
 parser = reqparse.RequestParser()
-
 
 def get_bucket(id, user_id):
     # Return the bucket from db using id
@@ -26,15 +26,7 @@ class BucketListsApi(Resource):
         if query:
             bucket = Bucketlist.query.filter(Bucketlist.name == query).first()
             if bucket:
-                response = jsonify({
-                    'id': bucket.id,
-                    'name': bucket.name,
-                    'date_created': bucket.date_created,
-                    'date_modified': bucket.date_modified,
-                    'owned_by': bucket.owned_by
-                })
-                response.status_code = 200
-                return response       
+                return marshal(bucket, bucketlist_serializer), 200
             else:
                 abort(404, message="Bucketlist with name '{}' doesn't exist".format(query))
         
@@ -42,18 +34,9 @@ class BucketListsApi(Resource):
             limit = int(limit)
             page = 1
             buckets = Bucketlist.query.filter_by(owned_by=user_id).paginate(page, limit, error_out=False)
-            print('this is the pagination object', dir(buckets))
-            print(buckets.items)
             results = []
             for bucket in buckets.items:
-                bucket_obj = {
-                    'id': bucket.id,
-                    'name': bucket.name,
-                    'date_created': bucket.date_created,
-                    'date_modified': bucket.date_modified,
-                    'owned_by': bucket.owned_by,
-                    'items': bucket.items
-                }
+                bucket_obj = marshal(bucket, bucketlist_serializer)
                 results.append(bucket_obj)
             response = jsonify(results)
             response.status_code = 200
@@ -63,14 +46,7 @@ class BucketListsApi(Resource):
             buckets = Bucketlist.get_all(user_id)
             results = []
             for bucket in buckets:
-                bucket_obj = {
-                    'id': bucket.id,
-                    'name': bucket.name,
-                    'date_created': bucket.date_created,
-                    'date_modified': bucket.date_modified,
-                    'owned_by': bucket.owned_by,
-                    'items': bucket.items
-                }
+                bucket_obj = marshal(bucket, bucketlist_serializer)
                 results.append(bucket_obj)
             response = jsonify(results)
             response.status_code = 200
@@ -84,16 +60,7 @@ class BucketListsApi(Resource):
         bucket = Bucketlist(name=args['name'], owned_by=user_id)
         bucket.save()
         # return the created bucketlist
-        response = jsonify({
-            'id': bucket.id,
-            'name': bucket.name,
-            'date_created': bucket.date_created,
-            'date_modified': bucket.date_modified,
-            'owned_by': bucket.owned_by,
-            'items': bucket.items
-        })
-        response.status_code = 201
-        return response
+        return marshal(bucket, bucketlist_serializer), 201
 
 
 class BucketListApi(Resource):
@@ -103,16 +70,7 @@ class BucketListApi(Resource):
         # return bucketlist with id from parameter
         abort_if_bucket_doesnt_exist(id, user_id)
         bucket = get_bucket(id, user_id)
-        response = jsonify({
-            'id': bucket.id,
-            'name': bucket.name,
-            'date_created': bucket.date_created,
-            'date_modified': bucket.date_modified,
-            'owned_by': bucket.owned_by,
-            'items': bucket.items
-        })
-        response.status_code = 200
-        return response
+        return marshal(bucket, bucketlist_serializer), 200
 
     @requires_auth
     def put(self, user_id, id):
@@ -123,7 +81,7 @@ class BucketListApi(Resource):
         bucket = get_bucket(id, user_id)
         bucket.name = args['name']
         bucket.save()
-        return "successfully updated the bucket"
+        return marshal(bucket, bucketlist_serializer), 201
 
     @requires_auth
     def delete(self, user_id, id):
